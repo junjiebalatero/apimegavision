a1abea8a72c801f664e290a2c9f2be85702f4386
Margarito Balatero <nycc40.yohoo.com.ph>
2021-06-06 11:19:11 +0800

    make it better

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-   MONGO_URI: 'mongodb+srv://junjie:8LETN2GxF98lZ2It@junjie-mern.p63jc.mongodb.net/kod?retryWrites=true&w=majority'   
+   MONGO_URI: 'mongodb+srv://junjie:8LETN2GxF98lZ2It@junjie-mern.p63jc.mongodb.net/megavision?retryWrites=true&w=majority'   
 }
